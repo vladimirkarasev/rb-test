c0b520aeef946a91711cb411ce11e0b6b3099295
Vladimir Karasev <vladimirkarasev9@gmail.com>
2024-04-10 20:26:40 +0300

    Добавил возможность добавлять команды

---
+++ app/src/Form/TeamType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Team;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null, [
+                'required' => true,
+                'label' => 'Название'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Сохранить',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Team::class,
+        ]);
+    }
+}
